fa0d23f4660c28b6896a1f3230f942697a5b66f1
Matthew Farrell <farrellm@alum.mit.edu>
2014-12-07 21:03:32 -0500

    add bootstrap init.el

---
+++ init.el
@@ -0,0 +1,12 @@
+;; bootstrapping init.el
+(package-initialize)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+
+;; packages:
+;;   auto-complete
+;;   cider
+;;   flycheck
+;;   nrepl
+;;   org
+;;   rainbow-delimiters
