36306df045bd3e023ef12513808e26d23aaec121
Matthew Farrell <farrellm@alum.mit.edu>
2016-12-13 21:26:55 -0500

    initial

---
+++ bootstrap.el
@@ -0,0 +1,27 @@
+;; org
+;; aggressive-indent
+;; adjust-parens
+;; company
+;; zenburn-theme
+;; smex
+
+;; evil
+;; smartparens
+
+;; auctex
+;; cider
+;; ess
+;; idris-mode
+;; intero
+
+;; hindent
+;; ido-ubiquitous
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/") t)
