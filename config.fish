6ae347e8240a4f9a4324e65fba819d00540c98b4
Matthew Farrell <farrellm@alum.mit.edu>
2017-01-13 00:04:08 -0500

    update fish config

--- config.fish
+++ config.fish
@@ -2,13 +2,17 @@
 # ~/.config/fish/config.fish
 #
 
-setenv EDITOR vim
-setenv PATH ~/bin ~/.local/bin $PATH
-setenv JAVA_HOME /usr/lib/jvm/java-7-openjdk
-
 # If not running interactively, don't do anything
 if not status --is-interactive
    exit
+end
+
+# setenv EDITOR vim
+setenv PATH ~/bin ~/.local/bin /usr/local/bin $PATH
+setenv JAVA_HOME /usr/lib/jvm/default
+
+if test "$TERM" = "xterm"
+    set -x TERM "xterm-256color"
 end
 
 # turn off audible bell
@@ -17,12 +21,7 @@ xset -b
 # aliases
 alias ls='ls --color=auto'
 alias ll='ls -l --color=auto'
-
 alias em='emacs -nw'
-
-alias rdesktop-ts='rdesktop -u mfarrell -d TWOSIGMA -z -g 1356x738 localhost:7777'
-alias rdesktop-ts2='rdesktop -u mfarrell -d TWOSIGMA -z -g 2048x1500 localhost:7777'
-
 
 alias git=hub
 
@@ -41,9 +40,5 @@ abbr -a grr 'git remote rm'
 abbr -a gpu 'git pull'
 abbr -a gcl 'git clone'
 
-if status --is-interactive;
-#	eval (keychain --eval --agents ssh id_rsa --quiet)
-#       [ -e $HOME/.keychain/(hostname)-fish ]; and . $HOME/.keychain/(hostname)-fish
-end
-
 source ~/.config/fish/prompt.fish
+eval sh ~/.config/fish/zenburn.sh
